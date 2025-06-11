2ea99b1663054515eda7bfb9f4e5257ff47d0bf6
Gopal <gopal.lohar.dev@gmail.com>
2025-06-11 23:15:02 +0530

    create docker and nginx based test server and a weird bug?

--- download_engine/src/download_config.rs
+++ download_engine/src/download_config.rs
@@ -9,7 +9,9 @@ pub struct DownloadConfig {
 impl Default for DownloadConfig {
     fn default() -> Self {
         Self {
-            buffer_size: 1024 * 1024,
+            // at buffer size 1024 * 100, download rate limited to 500KB/s in ngix
+            // a 50MB file is getting downloaded 50.59MB while if buffer size increased to 1024*1024 no problem
+            buffer_size: 1024 * 50,
             update_interval: 500,
             retry_count: 3,
             connections_per_server: 10,
