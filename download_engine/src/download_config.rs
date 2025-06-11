9b2b8ddbdb0e077ecec7418532a604288586a747
Gopal <gopal.lohar.dev@gmail.com>
2025-06-11 23:38:56 +0530

    i'm not fixing this bug, keep the buffer size big

--- download_engine/src/download_config.rs
+++ download_engine/src/download_config.rs
@@ -11,7 +11,7 @@ impl Default for DownloadConfig {
         Self {
             // at buffer size 1024 * 100, download rate limited to 500KB/s in ngix
             // a 50MB file is getting downloaded 50.59MB while if buffer size increased to 1024*1024 no problem
-            buffer_size: 1024 * 50,
+            buffer_size: 1024 * 1024,
             update_interval: 500,
             retry_count: 3,
             connections_per_server: 10,
